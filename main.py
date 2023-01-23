d9302e0ebd53e6968864d3e321e848fc1fe583ed
eriosta <eri@sienaspace.com>
2023-01-23 00:39:53 -0600

    update main.py

--- main.py
+++ main.py
@@ -38,18 +38,19 @@ def read_pdf(path,from_page=0, until_page=None):
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-prompt = read_pdf(path='pdfs\practice-exam-2013-questions-and-answers.pdf',from_page=11) + ". Describe the three categories of applications software"
+YOUR_QUESTION = "What is an incident-response plan?"
+
+prompt = read_pdf(path='pdfs\practice-exam-2013-questions-and-answers.pdf',from_page=11) + ". " + YOUR_QUESTION
 
 response = openai.Completion.create(
   model="text-davinci-003",
   prompt=prompt,
-  temperature=0,
-  max_tokens=50,
+  temperature=0.3,
+  max_tokens=100,
   top_p=1,
   frequency_penalty=0.5,
   presence_penalty=0
 )
-
 
 text = response['choices'][0]['text']
 print(text)
